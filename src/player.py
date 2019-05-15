b47f151015da2e3bef65ae236100304d370ac095
Joseph <josephryan96@gmail.com>
2019-05-14 19:57:24 -0700

    added items to player class

--- src/player.py
+++ src/player.py
@@ -3,8 +3,9 @@
 
 
 class Player:
-    def __init__(self, current_room):
+    def __init__(self, current_room, inv):
         self.current_room = current_room
+        self.inv = []
 
     def __repr__(self):
         return f"Player is in {self.current_room}"
