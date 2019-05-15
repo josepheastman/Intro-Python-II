62b02e6b719e37e244f34208e9ee56d14e9c21fe
Joseph <josephryan96@gmail.com>
2019-05-14 20:22:37 -0700

    added items to the player and rooms

--- src/adv.py
+++ src/adv.py
@@ -1,27 +1,28 @@
 
 from room import Room
 from player import Player
+from item import Item
 import textwrap
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -39,9 +40,11 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+room['outside'].items.append(
+    Item("sword", "This sword has a blade of a copper color, styled to resemble a fang. The guard is a pair of bird wings."))
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player(room['outside'])
+player = Player(room['outside'], [])
 
 # Write a loop that:
 #
@@ -68,6 +71,7 @@ def move_direction(direction, current_room):
 while True:
     print(player.current_room.name)
     print(player.current_room.description)
+    print(player.current_room.items)
     s = input("\n>").lower().split()
 
     if len(s) == 1:
